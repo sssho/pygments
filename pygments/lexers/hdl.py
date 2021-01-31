37d674bedbc54b7ca22b9b8d15872b72bb014f38
sssho <43515414+sssho@users.noreply.github.com>
2021-01-31 14:58:44 +0900

    Clean SystemVerilog lexer

--- pygments/lexers/hdl.py
+++ pygments/lexers/hdl.py
@@ -161,6 +161,148 @@ class SystemVerilogLexer(RegexLexer):
 
     lifetime = 'static|automatic'
 
+    keywords = (
+        'accept_on', 'alias', 'always', 'always_comb', 'always_ff',
+        'always_latch', 'and', 'assert', 'assign', 'assume', 'automatic',
+        'before', 'begin', 'bind', 'bins', 'binsof', 'break', 'buf',
+        'bufif0', 'bufif1', 'case', 'casex', 'casez', 'cell',
+        'checker', 'clocking', 'cmos', 'config',
+        'constraint', 'context', 'continue', 'cover', 'covergroup',
+        'coverpoint', 'cross', 'deassign', 'default', 'defparam', 'design',
+        'disable', 'do', 'edge', 'else', 'end', 'endcase',
+        'endchecker', 'endclocking', 'endconfig', 'endfunction',
+        'endgenerate', 'endgroup', 'endinterface',
+        'endprimitive', 'endprogram', 'endproperty', 'endsequence',
+        'endspecify', 'endtable', 'enum', 'eventually',
+        'expect', 'export', 'extern', 'final', 'first_match',
+        'for', 'force', 'foreach', 'forever', 'fork', 'forkjoin', 'function',
+        'generate', 'genvar', 'global', 'highz0', 'highz1', 'if', 'iff',
+        'ifnone', 'ignore_bins', 'illegal_bins', 'implies', 'implements', 'import',
+        'incdir', 'include', 'initial', 'inout', 'input',
+        'instance', 'interconnect', 'interface', 'intersect', 'join',
+        'join_any', 'join_none', 'large', 'let', 'liblist', 'library',
+        'local', 'localparam', 'macromodule', 'matches',
+        'medium', 'modport', 'nand', 'negedge', 'nettype', 'new', 'nexttime',
+        'nmos', 'nor', 'noshowcancelled', 'not', 'notif0', 'notif1', 'null',
+        'or', 'output', 'packed', 'parameter', 'pmos', 'posedge',
+        'primitive', 'priority', 'program', 'property', 'protected', 'pull0',
+        'pull1', 'pulldown', 'pullup', 'pulsestyle_ondetect',
+        'pulsestyle_onevent', 'pure', 'rand', 'randc', 'randcase',
+        'randsequence', 'rcmos', 'ref',
+        'reject_on', 'release', 'repeat', 'restrict', 'return', 'rnmos',
+        'rpmos', 'rtran', 'rtranif0', 'rtranif1', 's_always', 's_eventually',
+        's_nexttime', 's_until', 's_until_with', 'scalared', 'sequence',
+        'showcancelled', 'small', 'soft', 'solve',
+        'specify', 'specparam', 'static', 'strong', 'strong0',
+        'strong1', 'struct', 'super', 'sync_accept_on',
+        'sync_reject_on', 'table', 'tagged', 'this', 'throughout',
+        'timeprecision', 'timeunit', 'tran', 'tranif0', 'tranif1',
+        'typedef', 'union', 'unique', 'unique0', 'until',
+        'until_with', 'untyped', 'use', 'vectored',
+        'virtual', 'wait', 'wait_order', 'weak', 'weak0',
+        'weak1', 'while', 'wildcard', 'with', 'within',
+        'xnor', 'xor'
+    )
+
+    preprocs = (
+        '`__FILE__', '`__LINE__', '`begin_keywords', '`celldefine',
+        '`default_nettype', '`define', '`else', '`elsif', '`end_keywords',
+        '`endcelldefine', '`endif', '`ifdef', '`ifndef', '`include',
+        '`line', '`nounconnected_drive', '`pragma', '`resetall',
+        '`timescale', '`unconnected_drive', '`undef', '`undefineall'
+    )
+
+    builtins = (
+        # Simulation control tasks (20.2)
+        '$exit', '$finish', '$stop',
+        # Simulation time functions (20.3)
+        '$realtime', '$stime', '$time',
+        # Timescale tasks (20.4)
+        '$printtimescale', '$timeformat',
+        # Conversion functions
+        '$bitstoreal', '$bitstoshortreal', '$cast', '$itor',
+        '$realtobits', '$rtoi', '$shortrealtobits', '$signed',
+        '$unsigned',
+        # Data query functions (20.6)
+        '$bits', '$isunbounded', '$typename',
+        # Array query functions (20.7)
+        '$dimensions', '$high', '$increment', '$left', '$low', '$right',
+        '$size', '$unpacked_dimensions',
+        # Math functions (20.8)
+        '$acos', '$acosh', '$asin', '$asinh', '$atan', '$atan2',
+        '$atanh', '$ceil', '$clog2', '$cos', '$cosh', '$exp', '$floor',
+        '$hypot', '$ln', '$log10', '$pow', '$sin', '$sinh', '$sqrt',
+        '$tan', '$tanh',
+        # Bit vector system functions (20.9)
+        '$countbits', '$countones', '$isunknown', '$onehot', '$onehot0',
+        # Severity tasks (20.10)
+        '$info', '$error', '$fatal', '$warning',
+        # Assertion control tasks (20.12)
+        '$assertcontrol', '$assertfailoff', '$assertfailon',
+        '$assertkill', '$assertnonvacuouson', '$assertoff', '$asserton',
+        '$assertpassoff', '$assertpasson', '$assertvacuousoff',
+        # Sampled value system functions (20.13)
+        '$changed', '$changed_gclk', '$changing_gclk', '$falling_gclk',
+        '$fell', '$fell_gclk', '$future_gclk', '$past', '$past_gclk',
+        '$rising_gclk', '$rose', '$rose_gclk', '$sampled', '$stable',
+        '$stable_gclk', '$steady_gclk',
+        # Coverage control functions (20.14)
+        '$coverage_control', '$coverage_get', '$coverage_get_max',
+        '$coverage_merge', '$coverage_save', '$get_coverage',
+        '$load_coverage_db', '$set_coverage_db_name',
+        # Probabilistic distribution functions (20.15)
+        '$dist_chi_square', '$dist_erlang', '$dist_exponential',
+        '$dist_normal', '$dist_poisson', '$dist_t', '$dist_uniform',
+        '$random',
+        # Stochastic analysis tasks and functions (20.16)
+        '$q_add', '$q_exam', '$q_full', '$q_initialize', '$q_remove',
+        # PLA modeling tasks (20.17)
+        '$async$and$array', '$async$and$plane', '$async$nand$array',
+        '$async$nand$plane', '$async$nor$array', '$async$nor$plane',
+        '$async$or$array', '$async$or$plane', '$sync$and$array',
+        '$sync$and$plane', '$sync$nand$array', '$sync$nand$plane',
+        '$sync$nor$array', '$sync$nor$plane', '$sync$or$array',
+        '$sync$or$plane',
+        # Miscellaneous tasks and functions (20.18)
+        '$system',
+        # Display tasks (21.2)
+        '$display', '$displayb', '$displayh', '$displayo', '$monitor',
+        '$monitorb', '$monitorh', '$monitoro', '$monitoroff',
+        '$monitoron', '$strobe', '$strobeb', '$strobeh', '$strobeo',
+        '$write', '$writeb', '$writeh', '$writeo',
+        # File I/O tasks and functions (21.3)
+        '$fclose', '$fdisplay', '$fdisplayb', '$fdisplayh',
+        '$fdisplayo', '$feof', '$ferror', '$fflush', '$fgetc', '$fgets',
+        '$fmonitor', '$fmonitorb', '$fmonitorh', '$fmonitoro', '$fopen',
+        '$fread', '$fscanf', '$fseek', '$fstrobe', '$fstrobeb',
+        '$fstrobeh', '$fstrobeo', '$ftell', '$fwrite', '$fwriteb',
+        '$fwriteh', '$fwriteo', '$rewind', '$sformat', '$sformatf',
+        '$sscanf', '$swrite', '$swriteb', '$swriteh', '$swriteo',
+        '$ungetc',
+        # Memory load tasks (21.4)
+        '$readmemb', '$readmemh',
+        # Memory dump tasks (21.5)
+        '$writememb', '$writememh',
+        # Command line input (21.6)
+        '$test$plusargs', '$value$plusargs',
+        # VCD tasks (21.7)
+        '$dumpall', '$dumpfile', '$dumpflush', '$dumplimit', '$dumpoff',
+        '$dumpon', '$dumpports', '$dumpportsall', '$dumpportsflush',
+        '$dumpportslimit', '$dumpportsoff', '$dumpportson', '$dumpvars'
+    )
+
+    data_types = (
+        'bit', 'byte', 'chandle', 'const', 'event', 'int', 'integer',
+        'logic', 'longint', 'real', 'realtime', 'reg', 'shortint',
+        'shortreal', 'signed', 'string', 'time', 'type', 'unsigned',
+        'var', 'void'
+    )
+
+    net_types = (
+        'supply0', 'supply1', 'tri', 'triand', 'trior', 'trireg',
+        'tri0', 'tri1', 'uwire', 'wand', 'wire', 'wor'
+    )
+
     tokens = {
         'root': [
             (r'^\s*`define', Comment.Preproc, 'macro'),
@@ -197,49 +339,7 @@ class SystemVerilogLexer(RegexLexer):
             (r'(\$)(\s*)(\])', bygroups(Punctuation, Text, Punctuation)),
             (r'`[a-zA-Z_]\w*', Name.Constant),
 
-            (words((
-                'accept_on', 'alias', 'always', 'always_comb', 'always_ff',
-                'always_latch', 'and', 'assert', 'assign', 'assume', 'automatic',
-                'before', 'begin', 'bind', 'bins', 'binsof', 'break', 'buf',
-                'bufif0', 'bufif1', 'case', 'casex', 'casez', 'cell',
-                'checker', 'clocking', 'cmos', 'config',
-                'constraint', 'context', 'continue', 'cover', 'covergroup',
-                'coverpoint', 'cross', 'deassign', 'default', 'defparam', 'design',
-                'disable', 'do', 'edge', 'else', 'end', 'endcase',
-                'endchecker', 'endclocking', 'endconfig', 'endfunction',
-                'endgenerate', 'endgroup', 'endinterface',
-                'endprimitive', 'endprogram', 'endproperty', 'endsequence',
-                'endspecify', 'endtable', 'enum', 'eventually',
-                'expect', 'export', 'extern', 'final', 'first_match',
-                'for', 'force', 'foreach', 'forever', 'fork', 'forkjoin', 'function',
-                'generate', 'genvar', 'global', 'highz0', 'highz1', 'if', 'iff',
-                'ifnone', 'ignore_bins', 'illegal_bins', 'implies', 'implements', 'import',
-                'incdir', 'include', 'initial', 'inout', 'input',
-                'instance', 'interconnect', 'interface', 'intersect', 'join',
-                'join_any', 'join_none', 'large', 'let', 'liblist', 'library',
-                'local', 'localparam', 'macromodule', 'matches',
-                'medium', 'modport', 'nand', 'negedge', 'nettype', 'new', 'nexttime',
-                'nmos', 'nor', 'noshowcancelled', 'not', 'notif0', 'notif1', 'null',
-                'or', 'output', 'packed', 'parameter', 'pmos', 'posedge',
-                'primitive', 'priority', 'program', 'property', 'protected', 'pull0',
-                'pull1', 'pulldown', 'pullup', 'pulsestyle_ondetect',
-                'pulsestyle_onevent', 'pure', 'rand', 'randc', 'randcase',
-                'randsequence', 'rcmos', 'ref',
-                'reject_on', 'release', 'repeat', 'restrict', 'return', 'rnmos',
-                'rpmos', 'rtran', 'rtranif0', 'rtranif1', 's_always', 's_eventually',
-                's_nexttime', 's_until', 's_until_with', 'scalared', 'sequence',
-                'showcancelled', 'small', 'soft', 'solve',
-                'specify', 'specparam', 'static', 'strong', 'strong0',
-                'strong1', 'struct', 'super', 'sync_accept_on',
-                'sync_reject_on', 'table', 'tagged', 'this', 'throughout',
-                'timeprecision', 'timeunit', 'tran', 'tranif0', 'tranif1',
-                'typedef', 'union', 'unique', 'unique0', 'until',
-                'until_with', 'untyped', 'use', 'vectored',
-                'virtual', 'wait', 'wait_order', 'weak', 'weak0',
-                'weak1', 'while', 'wildcard', 'with', 'within',
-                'xnor', 'xor'),
-                suffix=r'\b'),
-             Keyword),
+            (words(keywords, suffix=r'\b'), Keyword),
 
             # package declaration
             (r'^(\s*)(package)(\s+)([a-zA-Z_]\w*)',
@@ -267,106 +367,10 @@ class SystemVerilogLexer(RegexLexer):
             (r'(endclass\b)(?:(\s*)(:)(\s*)([a-zA-Z_]\w*))?',
              bygroups(Keyword.Declaration, Text, Punctuation, Text, Name.Class)),
 
-            (words((
-                # Variable types
-                'bit', 'byte', 'chandle', 'const', 'event', 'int', 'integer',
-                'logic', 'longint', 'real', 'realtime', 'reg', 'shortint',
-                'shortreal', 'signed', 'string', 'time', 'type', 'unsigned',
-                'var', 'void',
-                # Net types
-                'supply0', 'supply1', 'tri', 'triand', 'trior', 'trireg',
-                'tri0', 'tri1', 'uwire', 'wand', 'wire', 'wor'),
-                suffix=r'\b'),
-             Keyword.Type),
-
-            (words((
-                '`__FILE__', '`__LINE__', '`begin_keywords', '`celldefine',
-                '`default_nettype', '`define', '`else', '`elsif', '`end_keywords',
-                '`endcelldefine', '`endif', '`ifdef', '`ifndef', '`include',
-                '`line', '`nounconnected_drive', '`pragma', '`resetall',
-                '`timescale', '`unconnected_drive', '`undef', '`undefineall'),
-                suffix=r'\b'),
-             Comment.Preproc),
-
-            (words((
-                # Simulation control tasks (20.2)
-                '$exit', '$finish', '$stop',
-                # Simulation time functions (20.3)
-                '$realtime', '$stime', '$time',
-                # Timescale tasks (20.4)
-                '$printtimescale', '$timeformat',
-                # Conversion functions
-                '$bitstoreal', '$bitstoshortreal', '$cast', '$itor',
-                '$realtobits', '$rtoi', '$shortrealtobits', '$signed',
-                '$unsigned',
-                # Data query functions (20.6)
-                '$bits', '$isunbounded', '$typename',
-                # Array query functions (20.7)
-                '$dimensions', '$high', '$increment', '$left', '$low', '$right',
-                '$size', '$unpacked_dimensions',
-                # Math functions (20.8)
-                '$acos', '$acosh', '$asin', '$asinh', '$atan', '$atan2',
-                '$atanh', '$ceil', '$clog2', '$cos', '$cosh', '$exp', '$floor',
-                '$hypot', '$ln', '$log10', '$pow', '$sin', '$sinh', '$sqrt',
-                '$tan', '$tanh',
-                # Bit vector system functions (20.9)
-                '$countbits', '$countones', '$isunknown', '$onehot', '$onehot0',
-                # Severity tasks (20.10)
-                '$info', '$error', '$fatal', '$warning',
-                # Assertion control tasks (20.12)
-                '$assertcontrol', '$assertfailoff', '$assertfailon',
-                '$assertkill', '$assertnonvacuouson', '$assertoff', '$asserton',
-                '$assertpassoff', '$assertpasson', '$assertvacuousoff',
-                # Sampled value system functions (20.13)
-                '$changed', '$changed_gclk', '$changing_gclk', '$falling_gclk',
-                '$fell', '$fell_gclk', '$future_gclk', '$past', '$past_gclk',
-                '$rising_gclk', '$rose', '$rose_gclk', '$sampled', '$stable',
-                '$stable_gclk', '$steady_gclk',
-                # Coverage control functions (20.14)
-                '$coverage_control', '$coverage_get', '$coverage_get_max',
-                '$coverage_merge', '$coverage_save', '$get_coverage',
-                '$load_coverage_db', '$set_coverage_db_name',
-                # Probabilistic distribution functions (20.15)
-                '$dist_chi_square', '$dist_erlang', '$dist_exponential',
-                '$dist_normal', '$dist_poisson', '$dist_t', '$dist_uniform',
-                '$random',
-                # Stochastic analysis tasks and functions (20.16)
-                '$q_add', '$q_exam', '$q_full', '$q_initialize', '$q_remove',
-                # PLA modeling tasks (20.17)
-                '$async$and$array', '$async$and$plane', '$async$nand$array',
-                '$async$nand$plane', '$async$nor$array', '$async$nor$plane',
-                '$async$or$array', '$async$or$plane', '$sync$and$array',
-                '$sync$and$plane', '$sync$nand$array', '$sync$nand$plane',
-                '$sync$nor$array', '$sync$nor$plane', '$sync$or$array',
-                '$sync$or$plane',
-                # Miscellaneous tasks and functions (20.18)
-                '$system',
-                # Display tasks (21.2)
-                '$display', '$displayb', '$displayh', '$displayo', '$monitor',
-                '$monitorb', '$monitorh', '$monitoro', '$monitoroff',
-                '$monitoron', '$strobe', '$strobeb', '$strobeh', '$strobeo',
-                '$write', '$writeb', '$writeh', '$writeo',
-                # File I/O tasks and functions (21.3)
-                '$fclose', '$fdisplay', '$fdisplayb', '$fdisplayh',
-                '$fdisplayo', '$feof', '$ferror', '$fflush', '$fgetc', '$fgets',
-                '$fmonitor', '$fmonitorb', '$fmonitorh', '$fmonitoro', '$fopen',
-                '$fread', '$fscanf', '$fseek', '$fstrobe', '$fstrobeb',
-                '$fstrobeh', '$fstrobeo', '$ftell', '$fwrite', '$fwriteb',
-                '$fwriteh', '$fwriteo', '$rewind', '$sformat', '$sformatf',
-                '$sscanf', '$swrite', '$swriteb', '$swriteh', '$swriteo',
-                '$ungetc',
-                # Memory load tasks (21.4)
-                '$readmemb', '$readmemh',
-                # Memory dump tasks (21.5)
-                '$writememb', '$writememh',
-                # Command line input (21.6)
-                '$test$plusargs', '$value$plusargs',
-                # VCD tasks (21.7)
-                '$dumpall', '$dumpfile', '$dumpflush', '$dumplimit', '$dumpoff',
-                '$dumpon', '$dumpports', '$dumpportsall', '$dumpportsflush',
-                '$dumpportslimit', '$dumpportsoff', '$dumpportson', '$dumpvars',
-                ), suffix=r'\b'),
-             Name.Builtin),
+            (words(data_types, suffix=r'\b'), Keyword.Type),
+            (words(net_types, suffix=r'\b'), Keyword.Type),
+            (words(preprocs, suffix=r'\b'), Comment.Preproc),
+            (words(builtins, suffix=r'\b'), Name.Builtin),
 
             (r'[a-zA-Z_]\w*:(?!:)', Name.Label),
             (r'\$?[a-zA-Z_]\w*', Name),
